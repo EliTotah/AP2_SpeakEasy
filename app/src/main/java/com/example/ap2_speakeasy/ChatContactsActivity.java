a0a081144da53ddd8bc167f840ff704ea0d45066
EliTotah <totah5034020@gmail.com>
2023-06-11 19:51:14 +0300

    update 11\06 20:00

---
+++ app/src/main/java/com/example/ap2_speakeasy/ChatContactsActivity.java
@@ -0,0 +1,56 @@
+package com.example.ap2_speakeasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ChatContactsActivity extends AppCompatActivity {
+
+    final private int[] profilePictures = {
+            R.drawable.kroos, R.drawable.neuer, R.drawable.ramos,
+            R.drawable.modric, R.drawable.muller,
+            R.drawable.mbappe, R.drawable.neymar
+    };
+
+    final private String[] userNames = {
+            "Toni Kroos", "Manuel Neuer", "Sergio Ramos", "Luka Modrić ", "Thomas Müller",
+            "Kylian Mbappe", "Neymar Jr"
+    };
+
+    final private String[] lastMassages = {
+            "Hi, how are you?", "24K Magic", "Missing Madrid :(", "Wanna hear a joke?", "Yo!",
+            "Well....", "Did you see the latest John Wick?"
+    };
+
+    final private String[] times = {
+            "12:00", "00:30", "03:23", "08:59", "12:23", "22:54", "11:47"
+    };
+
+    ListView listView;
+    CustomListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_contacts);
+
+        ArrayList<User> users = new ArrayList<>();
+
+        for (int i = 0; i < profilePictures.length; i++) {
+            User aUser = new User(
+                    userNames[i], profilePictures[i],
+                    lastMassages[i], times[i]
+            );
+
+            users.add(aUser);
+        }
+
+        listView = findViewById(R.id.list_view);
+        adapter = new CustomListAdapter(getApplicationContext(), users);
+
+        listView.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/ap2_speakeasy/MainActivity.java
+++ app/src/main/java/com/example/ap2_speakeasy/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.ap2_speakeasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int DELAY_TIME_MS = 3000; // 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Use a Handler to introduce a delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the LoginActivity after the delay
+                Intent intent = new Intent(MainActivity.this, ChatContactsActivity.class);
+                startActivity(intent);
+                finish(); // Optional: Close the MainActivity so the user can't go back to it
+            }
+        }, DELAY_TIME_MS);
     }
 }
