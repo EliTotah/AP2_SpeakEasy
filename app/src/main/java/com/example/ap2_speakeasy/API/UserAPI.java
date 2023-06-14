a101bfe7d78f51250f5391fb66da12d30e202b36
EliTotah <totah5034020@gmail.com>
2023-06-14 21:42:46 +0300

    update 14\06 22:00 - eli (login and signup server works)

--- app/src/main/java/com/example/ap2_speakeasy/API/UserAPI.java
+++ app/src/main/java/com/example/ap2_speakeasy/API/UserAPI.java
@@ -1,14 +1,17 @@
 package com.example.ap2_speakeasy.API;
 
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.example.ap2_speakeasy.AP2_SpeakEasy;
 import com.example.ap2_speakeasy.LoginActivity;
+import com.example.ap2_speakeasy.MainActivity;
 import com.example.ap2_speakeasy.R;
 import com.example.ap2_speakeasy.Sign_up.PasswordActivity;
+import com.example.ap2_speakeasy.Sign_up.SignUpActivity;
 
 import java.io.IOException;
 import java.util.Map;
@@ -24,8 +27,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UserAPI {
-    Retrofit retrofit;
-    UserServiceAPI userServiceAPI;
+    private Retrofit retrofit;
+    private UserServiceAPI userServiceAPI;
+    public String token;
 
     public UserAPI() {
         /////////////
@@ -44,31 +48,34 @@ public class UserAPI {
         userServiceAPI = retrofit.create(UserServiceAPI.class);
     }
 
-    public Call<Map<String, String>> login(String username, String password) {
-        return userServiceAPI.login(Map.of("username", username, "password", password));
-    }
-
-//    public Call<Map<String, String>> login(String username, String password, String token) {
-//        return userServiceAPI.login(Map.of("username", username, "password", password, "firebaseToken", token));
-//    }
-//    public Call<ResponseBody> signup(String username, String password, String name, String profilePicture) {
-//        return userServiceAPI.signup(Map.of("username", username, "password", password, "displayName", name, "profilePic", profilePicture));
-//    }
 
     public void register(String username, String password, String name, String profilePicture, CallBackFlag callBackFlag) {
         Call<ResponseBody> signupCall = userServiceAPI.signup(Map.of("username", username, "password", password, "displayName", name, "profilePic", profilePicture));
         signupCall.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if (response.isSuccessful()) {
-                    callBackFlag.complete(true);
-                } else {
-                    int statusCode = response.code();
-//                    if (statusCode == 409) {
-//                        // User already exists, show appropriate message to the user using Toast
-//                        //Toast.makeText(getApplicationContext(), "This user already exists and cannot be created.", Toast.LENGTH_SHORT).show();
+                callBackFlag.complete(response.code());
+            }
 
-                    callBackFlag.complete(true);
+            @Override
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                //binding.editTextUsername.setError(getString(R.string.connection_error));
+            }
+        });
+    }
+
+    public void signIn(String username, String password,CallBackFlag callBackFlag) {
+        Call<ResponseBody> loginCall = userServiceAPI.login(Map.of("username", username, "password", password));
+        loginCall.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                callBackFlag.complete(response.code());
+                if (response.code() == 200) {
+                    try {
+                        token = response.body().string();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
 
