5361c18e39f150da7b1120809e370fe5edb3daee
EliTotah <totah5034020@gmail.com>
2023-06-19 12:23:13 +0300

    update 19\06 12:30 - eli

---
+++ app/src/main/java/com/example/ap2_speakeasy/ViewModels/MessageViewModel.java
@@ -0,0 +1,40 @@
+package com.example.ap2_speakeasy.ViewModels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.ap2_speakeasy.entities.Contact;
+import com.example.ap2_speakeasy.entities.Message;
+import com.example.ap2_speakeasy.repositories.ContactRepository;
+import com.example.ap2_speakeasy.repositories.MessageRepository;
+
+import java.util.List;
+
+public class MessageViewModel extends ViewModel {
+    private MessageRepository messageRepository;
+    private LiveData<List<Message>> messages;
+
+
+    public MessageViewModel(String token, int chatID) {
+        this.messageRepository = new MessageRepository(token, chatID);
+        this.messages = messageRepository.getAll();
+    }
+
+    public LiveData<List<Message>> getMessages() {
+        return this.messages;
+    }
+
+    // Get contact by id
+    /*public Contact getContact(int id) {
+        return contactsRepository.getContact(id);
+    }*/
+
+    public void insertMessage(String content) {
+        messageRepository.insertMessage(content);
+        //reload();
+    }
+
+    public void reload() {
+        this.messages = messageRepository.getAll();
+    }
+}
